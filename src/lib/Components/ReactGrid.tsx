1185b8f6dcc0988c099387322330588fb7ec7eca
David Barnes <dcbarnez@gmail.com>
2025-06-23 21:48:17 -0400

    chore: add ts-nocheck to the top of tsx files

--- src/lib/Components/ReactGrid.tsx
+++ src/lib/Components/ReactGrid.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import * as React from "react";
 import { CellRenderer, ReactGridProps } from "../../core";
 import {
--- src/lib/Components/SelectedRanges.tsx
+++ src/lib/Components/SelectedRanges.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import * as React from "react";
 import { PaneContentChild, Range } from "../../core";
 import { PartialArea } from "./PartialArea";
